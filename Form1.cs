61e73e64640855aaeb4c5349bb6addfbf206ef8f
BigBoss1 <alexol5452@gmail.com>
2025-04-18 13:31:10 +0200

    ui.ver2

--- Form1.cs
+++ Form1.cs
@@ -31,15 +31,13 @@ namespace IVS_proj2
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
         
-        }
-
-        }
         private void button1_Click(object sender, EventArgs e)
         {
 
+        }
 
-        private void Form1_Load_1(object sender, EventArgs e)
         private void button6_Click(object sender, EventArgs e)
+        {
 
         }
     }
