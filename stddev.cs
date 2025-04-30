a064ebd02ce7ff312114e637222a3fc591cf217b
B1GBoSS1 <alexol5452@gmail.com>
2025-04-30 16:49:10 +0200

    Created Standard Deviation Program

---
+++ stddev.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Stddev
+{
+	public Stddev()
+	{
+		int x_special = 0;
+		int x_i = 0;
+		int n = 0;
+
+		int s = 0;
+        int n-1 = 0;
+		int x_i_squared = 0;
+        int x_spec_squared = 0;
+		int N_x_spec_squared = 0;
+
+        string input_nums;
+        testString = Console.ReadLine();
+        Console.WriteLine("You entered '{0}'", input_nums);
+    
+
+
+
+    }
+}
